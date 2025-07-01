36422120c59600142e9c3f57202287be6487c953
mprytula <dev.mprytula@gmail.com>
2025-07-01 22:29:22 +0200

    Added LSPs and Prettier to Mason

--- lua/mprytula/lazy/mason_lsp.lua
+++ lua/mprytula/lazy/mason_lsp.lua
@@ -1,10 +1,10 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-        ensure_installed = { "lua_ls", "ts_ls", "biome@1.9.4", "eslint" }
-    },
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-    },
+	"mason-org/mason-lspconfig.nvim",
+	opts = {
+		ensure_installed = { "lua_ls", "ts_ls", "biome@1.9.4", "eslint", "eslint_d", "prettier" },
+	},
+	dependencies = {
+		{ "mason-org/mason.nvim", opts = {} },
+		"neovim/nvim-lspconfig",
+	},
 }
