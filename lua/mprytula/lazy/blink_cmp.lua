783b1006fe447a1be3378df39de65c48d0cab37d
mprytula <dev.mprytula@gmail.com>
2025-07-04 00:20:07 +0200

    Added label description to cmp

--- lua/mprytula/lazy/blink_cmp.lua
+++ lua/mprytula/lazy/blink_cmp.lua
@@ -34,7 +34,14 @@ return {
 		},
 
 		-- (Default) Only show the documentation popup when manually triggered
-		completion = { documentation = { auto_show = false } },
+		completion = {
+			documentation = { auto_show = true, auto_show_delay_ms = 500 },
+			menu = {
+				draw = {
+					columns = { { "kind_icon", "label", "label_description", gap = 1 } },
+				},
+			},
+		},
 
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
