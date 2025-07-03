52cb9f77342a56d9a173bb8875b4f65c118dfeb0
mprytula <dev.mprytula@gmail.com>
2025-07-03 11:38:42 +0200

    Code action keymap

--- lua/mprytula/lazy/lspconfig.lua
+++ lua/mprytula/lazy/lspconfig.lua
@@ -1,8 +1,18 @@
 return {
-    "neovim/nvim-lspconfig",
-    config = function()
-        local lspconfig = require("lspconfig")
-        lspconfig.ts_ls.setup {}
-        lspconfig.lua_ls.setup {}
-    end,
+	"neovim/nvim-lspconfig",
+	config = function()
+		local lspconfig = require("lspconfig")
+		lspconfig.ts_ls.setup({
+			on_attach = function(client, bufnr)
+				-- Enable code actions
+				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code Actions" })
+			end,
+		})
+		lspconfig.lua_ls.setup({
+			on_attach = function(client, bufnr)
+				-- Enable code actions
+				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code Actions" })
+			end,
+		})
+	end,
 }
